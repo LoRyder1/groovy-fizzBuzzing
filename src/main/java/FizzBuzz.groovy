7fe1534032c28f12caecbf208ff3de21ed410fbd
Lovinder Pnag <lspnag@gmail.com>
2016-03-18 00:57:57 -0400

    test for an array of numbers

--- src/main/java/FizzBuzz.groovy
+++ src/main/java/FizzBuzz.groovy
@@ -4,8 +4,8 @@ class FizzBuzz {
 //    def fizzBuzz = { int num -> return num.toString() }
     def fizzBuzz = {
         String result = ''
-        if (it % 3 == 0) { result = "Fizz" }
-        if (it % 5 == 0) { result = "Buzz" }
+        if (it % 3 == 0) { result += "Fizz" }
+        if (it % 5 == 0) { result += "Buzz" }
         if (!result) { result = it.toString() }
         result
     }
--- test/FizzBuzzTest.groovy
+++ test/FizzBuzzTest.groovy
@@ -3,12 +3,18 @@ import org.junit.Test
 
 class FizzBuzzTest {
 
-    private FizzBuzz num
+    private FizzBuzz num = new FizzBuzz()
 
-    @Before
-    void setUp() {
-        num = new FizzBuzz()
-    }
+    private fizzBuzzes = (1..15).collect { num.fizzBuzz(it) }
+
+    private expectedFizzBuzzes = [
+            '1', '2', 'Fizz', '4', 'Buzz', 'Fizz', '7', '8', 'Fizz',
+            'Buzz', '11', 'Fizz', '13', '14', 'FizzBuzz']
+
+//    @Before
+//    void setUp() {
+//        num = new FizzBuzz()
+//    }
 
     @Test
     void passANumberToFizzBuzzAndReturnNumber() {
@@ -21,7 +27,17 @@ class FizzBuzzTest {
     }
 
     @Test
-    void passAMultipleOfFIveAndReturnBuzz() {
+    void passAMultipleOfFiveAndReturnBuzz() {
         assert num.fizzBuzz(5) == "Buzz"
+    }
+
+    @Test
+    void passAMultipleOfFiveAndThreeAndReturnFizzBuzz() {
+        assert num.fizzBuzz(15) == "FizzBuzz"
+    }
+
+    @Test
+    void passACollectionOfNumbersAndReturnFizzBuzzesCorrectly() {
+        assert fizzBuzzes == expectedFizzBuzzes
     }
 }
