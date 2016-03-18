ec6667800cceea1805c880398dc68efd798e5a05
Lovinder Pnag <lspnag@gmail.com>
2016-03-18 01:24:14 -0400

    not using closure

--- src/main/java/FizzBuzz.groovy
+++ src/main/java/FizzBuzz.groovy
@@ -2,6 +2,7 @@
 class FizzBuzz {
 
 //    def fizzBuzz = { int num -> return num.toString() }
+
     def fizzBuzz = {
         String result = ''
         if (it % 3 == 0) { result += "Fizz" }
@@ -9,4 +10,28 @@ class FizzBuzz {
         if (!result) { result = it.toString() }
         result
     }
+
+
+//    def fizzBuzz(int num) {
+//        String result = ''
+//        if (isMultipleOfThree(num)) {
+//            result += 'Fizz'
+//        }
+//        if (isMultipleOfFive(num)) {
+//            result += 'Buzz'
+//        }
+//        if (!result) {
+//            result = num.toString()
+//        }
+//        result
+//    }
+
+    private static boolean isMultipleOfFive(int num) {
+        num % 5 == 0
+    }
+
+    private static boolean isMultipleOfThree(int num) {
+        num % 3 == 0
+    }
 }
+
